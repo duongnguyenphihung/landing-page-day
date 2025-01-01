8ea42af16622cf81861b0663d683586eab021505
thanhbm <manhthanh147@gmail.com>
2025-01-01 21:41:05 +0700

    hello

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import {index, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+    index("routes/landing-page.tsx"),
     route("landing", "routes/landing-page.tsx"),
 ] satisfies RouteConfig;
